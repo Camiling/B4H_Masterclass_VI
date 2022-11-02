1eaf240d0ee335cd8c1bf33ebd8e1f724fa02168
Camilla Lingjærde <camilla@Camillas-MBP.lan>
2022-11-02 18:39:49 +0000

    Add exercise 3

--- R_code/Ex3_VBEM.R
+++ R_code/Ex3_VBEM.R
@@ -1,4 +1,4 @@
-## Practical exercise 2: Bayesian Linear Regression
+## Practical exercise 3: Linear regression with empirical Bayes estimation for hyperparameters
 rm(list=ls())
 library(matrixcalc)
 
@@ -13,29 +13,44 @@ y = X %*% beta  + rnorm(n, sd = sqrt(1/phi)) # Generate y
 # Look at density
 plot(density(y))
 
-# Find mean-field estimates using CAVI
+# Initialize
 a0=b0=0.001
 E_kappa = a0/b0 # Initial value
+# Initial value
+phi_hat =0.2
 a = a0+p/2
 more=TRUE
+more_phi = TRUE
 XX = crossprod(X,X)
 Xy = crossprod(X,y)
 yy = c(crossprod(y))
 eps=0.0001
 m_old = c(1000,1000)
+phi_old = 1000
 
-while(more){
-  S = solve(diag(E_kappa, p) + phi * XX)
-  m = phi * S %*% Xy
-  E_betabeta =  as.numeric(crossprod(m) + matrixcalc::matrix.trace(S))
-  b = b0 + 0.5 * E_betabeta
-  E_kappa = a / b
-  # Assessing convergence by variational factors (can also use ELBO)
-  if(sum((m-m_old)^2)<eps){
-    more =FALSE
+while(more_phi){
+  # E-step
+  while(more){
+    S = solve(diag(E_kappa, p) + phi_hat * XX)
+    m = phi_hat * S %*% Xy
+    E_betabeta =  as.numeric(crossprod(m) + matrixcalc::matrix.trace(S))
+    b = b0 + 0.5 * E_betabeta
+    E_kappa = a / b
+    # Assessing convergence by variational factors (can also use ELBO)
+    if(sum((m-m_old)^2)<eps){
+      more =FALSE
+    }
+    m_old = m
   }
-  m_old = m
+  # M-step
+  phi_hat = as.numeric(n/(yy + matrixcalc::matrix.trace(XX%*%(m%*%t(m) + S)) - 2*t(m)%*%Xy))
+  # Check for convergence
+  more_phi = abs(phi_old-phi_hat) >= eps
+  phi_old = phi_hat
 }
+
+# Resulting estimate for phi
+phi_hat
 
 # Visualise bivariate Gaussian approximation for coefficients
 library(mnormt)
